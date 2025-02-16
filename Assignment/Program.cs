565f464bfc4d930cb87ddff17d8bd672d186a7ff
Nicholas Page <npage@southhills.edu>
2025-02-16 14:01:27 -0500

    Initial commit

---
+++ Assignment/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+
+namespace Assignment;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
